4c754dfec7c2f292f02b59bb9a94d792a12d57fd
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2024-03-06 00:52:12 +0200

    finish

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,49 @@
+import HttpError from '../helpers/HttpError.js';
+import ctrlWrapper from '../helpers/ctrlWrapper.js';
+
+import contactsServices from '../services/contactsServices.js';
+
+const getAllContacts = async (_, res) => {
+  const result = await contactsServices.listContacts();
+  res.json(result);
+};
+
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.getContactById(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.removeContact(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ message: 'Delete succes' });
+};
+
+const createContact = async (req, res) => {
+  const result = await contactsServices.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.updateContactById(id, req.body);
+  if (!result) {
+    throw HttpError(404, error.message);
+  }
+  res.json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+};
---
+++ routes/contactsRouter.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import contactsControllers from '../controllers/contactsControllers.js';
+import validateBody from '../helpers/validateBody.js';
+import contactsSchemas from '../schemas/contactsSchemas.js';
+
+const contactsRouter = express.Router();
+
+contactsRouter.get('/', contactsControllers.getAllContacts);
+
+contactsRouter.get('/:id', contactsControllers.getOneContact);
+
+contactsRouter.delete('/:id', contactsControllers.deleteContact);
+
+contactsRouter.post(
+  '/',
+  validateBody(contactsSchemas.createContactSchema),
+  contactsControllers.createContact
+);
+
+contactsRouter.put(
+  '/:id',
+  validateBody(contactsSchemas.updateContactSchema),
+  contactsControllers.updateContact
+);
+
+export default contactsRouter;
